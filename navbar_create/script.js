61d61aa7836896673a896d56c76d6e15f1e30f48
Umersaleem109 <umer.saleem.abbasi109@gmail.com>
2024-03-14 14:50:46 +0500

    side navbar design

---
+++ navbar_create/script.js
@@ -0,0 +1,9 @@
+$(function() {
+    // Sidebar toggle behavior
+    $('#sidebarCollapse').on('click', function() {
+      $('#sidebar, #content').toggleClass('active');
+    });
+  });
+
+  // for more content like this please follow us on
+  // https://www.instagram.com/web_deves
